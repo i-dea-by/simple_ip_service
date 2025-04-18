7c502f4c52745f96c318f2c4f05333b43df59616
egor daniloff <i@dea.by>
2025-04-18 23:32:56 +0300

    refactor: add commennt BASE_URL without trailing slash

--- src/config.py
+++ src/config.py
@@ -4,4 +4,4 @@ BASE_DIR = Path(__file__).resolve().parent
 TEMPLATES_DIR = BASE_DIR / "templates"
 STATIC_DIR = BASE_DIR / "static"
 
-BASE_URL = "http://localhost:8000"
+BASE_URL = "http://localhost:8000"  # without trailing slash
