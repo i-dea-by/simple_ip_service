c920f66620b1e2a9378593edb503c8116df841f9
egor daniloff <i@dea.by>
2025-04-19 23:33:27 +0300

    feat: add get env variable "BASE_URL" or predefined "http://localhost:8000"

--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
+from os import getenv
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent
 TEMPLATES_DIR = BASE_DIR / "templates"
 STATIC_DIR = BASE_DIR / "static"
 
-BASE_URL = "http://localhost:8000"  # without trailing slash
+BASE_URL = getenv("BASE_URL") or "http://localhost:8000" # without trailing slash
